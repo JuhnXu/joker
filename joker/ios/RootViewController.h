72a64acbef33af40145b2b348d90fbd2516f9a65
JuhnXu <JuhnXu@qq.com>
2013-05-30 15:51:52 +0800

    Initial Commit

---
+++ joker/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  jokerAppController.h
+//  joker
+//
+//  Created by JuhnXu on 13-5-30.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
